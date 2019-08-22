ca585a87a501503c6778e4ba47675ed0f01c797b
Henry <henrichukwudi@gmail.com>
2019-08-22 08:37:19 +0100

    login modified

--- edubin/js/login.js
+++ edubin/js/login.js
@@ -1,35 +1,28 @@
-$(window, document, undefined).ready(function() {
+let passwordInput = document.getElementById('txtPassword'),
+    toggle = document.getElementById('btnToggle'),
+    icon =  document.getElementById('eyeIcon');
 
-    $('input').blur(function() {
-        var $this = $(this);
-        if ($this.val())
-            $this.addClass('used');
-        else
-            $this.removeClass('used');
-    });
+function togglePassword() {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.add("fa-eye-slash");
+        //toggle.innerHTML = 'hide';
+    } else {
+        passwordInput.type = 'password';
+        icon.classList.remove("fa-eye-slash");
+        //toggle.innerHTML = 'show';
+    }
+}
 
-    var $ripples = $('.ripples');
+function checkInput() {
+    //if (passwordInput.value === '') {
+    //toggle.style.display = 'none';
+    //toggle.innerHTML = 'show';
+    //  passwordInput.type = 'password';
+    //} else {
+    //  toggle.style.display = 'block';
+    //}
+}
 
-    $ripples.on('click.Ripples', function(e) {
-
-        var $this = $(this);
-        var $offset = $this.parent().offset();
-        var $circle = $this.find('.ripplesCircle');
-
-        var x = e.pageX - $offset.left;
-        var y = e.pageY - $offset.top;
-
-        $circle.css({
-            top: y + 'px',
-            left: x + 'px'
-        });
-
-        $this.addClass('is-active');
-
-    });
-
-    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
-        $(this).removeClass('is-active');
-    });
-
-});
+toggle.addEventListener('click', togglePassword, false);
+passwordInput.addEventListener('keyup', checkInput, false);
